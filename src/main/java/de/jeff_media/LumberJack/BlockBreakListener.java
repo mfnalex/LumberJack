aa7f8954d0bb904a1230196ac7e3eef638e14925
mfnalex <mail@jeff-media.de>
2020-10-21 00:05:15 +0200

    2.3.3 release

--- src/main/java/de/jeff_media/LumberJack/BlockBreakListener.java
+++ src/main/java/de/jeff_media/LumberJack/BlockBreakListener.java
@@ -13,6 +13,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
 
 public class BlockBreakListener implements Listener {
 
@@ -22,7 +24,7 @@ public class BlockBreakListener implements Listener {
 		this.plugin = plugin;
 	}
 
-	@EventHandler
+	@EventHandler(ignoreCancelled = true)
 	public void onBlockBreak(BlockBreakEvent e) {
 		
 		// checking in lower case for lazy admins
